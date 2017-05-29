61b4817b6ca434b942f6527425afcd52297ebee9
Armon Azizi <armonazizi@gmail.com>
2017-05-28 20:35:05 -0700

    prosite document and updated script

--- prosite_script.py
+++ prosite_script.py
@@ -6,22 +6,39 @@ Script for querying prosite
 
 from Bio import SeqIO
 from Bio.ExPASy import ScanProsite
+from Bio import ExPASy
+from Bio.ExPASy import Prodoc
 
+
+
+records = open("prosite.doc")
+
+keys = {}
+
+for line in records:
+    if line.startswith("{PS"):
+        mid = line.find(';')
+        keys[line[1:mid]] = line[mid+2:len(line)-2]
+        
 
 file = SeqIO.parse("newDNA.txt", "fasta")
 
 output = open("output.txt", 'w')
 
-
 for sequence in file:
     
    output.write('>' + sequence.id + '\n')
+   
+   print '>' + sequence.id
    
    handle = ScanProsite.scan(seq=sequence.seq)
    
    result = ScanProsite.read(handle)
    
-   output.write(str(result) + '\n')
+   for record in result:
+       output.write(keys[record['signature_ac']]+'\n')
+       
+       print keys[record['signature_ac']]
 
 
 
