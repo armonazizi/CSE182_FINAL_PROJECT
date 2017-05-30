fa93d969ed6e7c7126669e7a15eb3de06af86da2
Armon Azizi <armonazizi@gmail.com>
2017-05-29 19:31:30 -0700

    tsv script

---
+++ makeTSV.py
@@ -0,0 +1,60 @@
+'''
+Make keyword tsv file from script output
+'''
+
+import subprocess
+
+subprocess.call(['python', 'prosite_script.py'])
+
+
+
+
+prosite_keys = open("prosite_keywords.txt", 'r')
+
+pfam_keys = open("Pfam_keywords.txt", 'r')
+
+blast_keys = open("BLAST.txt", 'r')
+
+#fasta = open("DNA.txt")
+
+prosite_arr = [""]*100
+pfam_arr = [""]*100
+blast_arr = [""]*100
+seq_ids = [""]*100
+
+counter=0
+
+for line in prosite_keys:
+    if line.startswith('>'):
+        seq_ids[counter] = ((line.split(':')[1]).split(' ')[0]).strip()
+    else:
+        prosite_arr[counter] = line.strip()
+        counter += 1
+
+counter = 0
+
+for line in pfam_keys:
+    if not line.startswith('>'):
+        pfam_arr[counter] = line.strip()
+        counter += 1
+
+counter = 0
+
+for line in blast_keys:
+    if not line.startswith('>'):
+        blast_arr[counter] = line.strip()
+        counter += 1
+        
+        
+out_file = open("keywords.tsv", 'w')
+
+out_file.write("SEQUENCE_ID\tBLAST_KEYS\tPFAM_KEYS\tPROSITE_KEYS\n")
+
+for i in range(0, 100):
+    out_file.write(seq_ids[i]+'\t'+blast_arr[i]+
+                   '\t'+pfam_arr[i]+'\t'+prosite_arr[i]+'\n')
+
+out_file.close()    
+
+
+
